f52932e95e07f0e88d9ea384a3d7178e9b7967dc
Joao Henrique Machado Silva <joaoh82@gmail.com>
2019-04-21 23:15:53 +0200

    Added comments to closure

--- closures.js
+++ closures.js
@@ -1,7 +1,9 @@
-var me = 'Bruce Wayne'
+// A Closure is the combination of the scope of a function and
+// the lexical environment within which that function was declared
+var me = 'Bruce Wayne' // Declaring variable me
 console.log('me', me)
 function greetMe() {
-    console.log('Hello, ' + me + '!')
+    console.log('Hello, ' + me + '!') // Using variable me with the value from the moment it was called
 }
 me = 'Batman'
 greetMe()
