26d37975c1b1ae003948f3ba0fd1b2f4c2b220a3
João Henrique Machado Silva <joao.henrique-silva@dazn.com>
2019-07-08 09:32:09 +0200

    Promise example added

--- promises.js
+++ promises.js
@@ -3,3 +3,9 @@
 //
 // The real benefits of Promises comes from composing Promises across the application
 // We can create functions that return Promises, and let errors bubble upstream.
+
+const asyncFunction = () => new Promise(resolve => setTimeout(resolve, 5000));
+
+asyncFunction()
+.then(() => console.log("Promise Resolved"))
+.catch(()=> console.log("fail"));
