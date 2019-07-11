5a7898ac7feb223465ca2dbc465fb3fcae4932df
João Henrique Machado Silva <joao.henrique-silva@dazn.com>
2019-07-11 15:31:41 +0200

    VS Code User Snippets added

--- fetch.js
+++ fetch.js
@@ -1,34 +1,38 @@
-const url = "https://api.somerandomapi.com.br/users"
+const url = 'https://api.somerandomapi.com.br/users';
 
 // Chaining promises all in one
 fetch(url).then(response => {
-    console.log(`Status: ${response.status}`)
+  console.log(`Status: ${response.status}`);
 
-    response.json().then(data => {
-        console.log('FETCH result:', data)
-    }).catch(err => {
-        console.error('FETCH Parsing Error', err)
+  response
+    .json()
+    .then(data => {
+      console.log('FETCH result:', data);
     })
-})
+    .catch(err => {
+      console.error('FETCH Parsing Error', err);
+    });
+});
 
 // Separating responsabilities
 fetch(url)
-    .then(checkStatus)
-    .then(getJSON)
-    .then(data => {
-        console.log('DATA', data)
-    }).catch(err => {
-        console.log('ERROR', err)
-    })
+  .then(checkStatus)
+  .then(getJSON)
+  .then(data => {
+    console.log('DATA', data);
+  })
+  .catch(err => {
+    console.log('ERROR', err);
+  });
 
 const checkStatus = response => {
-    if (response.status === 200) {
-        return Promise.resolve(response)
-    } else {
-        return Promise.reject( new Error(response.statusText))
-    }
-}
+  if (response.status === 200) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+};
 
 const getJSON = response => {
-    return response.json()
-}
+  return response.json();
+};
